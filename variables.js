078f11b1347c39dc7f859e0a8351ab2ed39fc95b
Kara Anderson <kanderson8585@gmail.com>
2022-10-14 12:24:11 -0600

    Reassign variable values

--- variables.js
+++ variables.js
@@ -7,3 +7,15 @@ var daysOfOctober = 31
 
 var isMyDogCute = true
 var isMyDogSmart = false
+
+var fish = "Tetra"
+var dog = "Dachshund"
+var cat = "Ragdoll"
+
+console.log(fish)
+console.log(dog)
+console.log(cat)
+console.log(daysOfWeek)
+console.log(daysOfOctober)
+console.log(isMyDogCute)
+console.log(isMyDogSmart)
